634144c878f6b87accf6edd2ff7fb036dd385667
Oleg Ilin <ilindev78@gmal.com>
2023-03-18 22:24:08 +0300

    upd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
